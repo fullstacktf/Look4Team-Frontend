a202cb4bc0d1f086904996b70266dfdacd92c19a
Jesús Ramos <jesusramosdev@gmail.com>
2019-12-09 21:42:04 +0000

    Borrado el directorio de scripts

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scp -o StrictHostKeyChecking=no -i deploy-travis -r build ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}
-
-# COMMAND="cd ${REMOTE_PATH} && git pull"
-
-# ssh -o StrictHostKeyChecking=no -i deploy-travis -v ${REMOTE_USER}@${REMOTE_HOST} ${COMMAND}
--- scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_47a6e7674e1d_key -iv $encrypted_47a6e7674e1d_iv -in travis.enc -out deploy-travis -d
-
-eval $(ssh-agent -s)
-
-chmod 600 deploy-travis
-
-ssh-add deploy-travis
