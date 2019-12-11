267089d003e8010436e04e1c771d976a95e6a4ae
Jesús Ramos <jesusramosdev@gmail.com>
2019-12-11 17:43:11 +0000

    Corregido error deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 tar -czf front.tgz build && \
 scp -o StrictHostKeyChecking=no -i deploy-travis -v -r front.tgz ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH} && \
-ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
+ssh -o StrictHostKeyChecking=no -i deploy-travis -v $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/untar.sh
 
 # COMMAND="cd ${REMOTE_PATH} && git pull"
 
